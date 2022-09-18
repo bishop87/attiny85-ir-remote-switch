fe83e6ab3ee34579e8aa836a373c01c6eb231f19
Bishop87 <bishop87@gmail.com>
2022-09-18 22:05:16 +0200

    added led status to code

--- attiny85-ir-remote-switch.ino
+++ attiny85-ir-remote-switch.ino
@@ -2,7 +2,8 @@
 #include <EEPROM.h>
 
 #define IR_RECEIVE_PIN 2
-#define IR_LEARN_PIN 3
+#define IR_LEARN_SW 3
+#define IR_LEARN_LED 4
 
 #define OUT_PIN 9
 
@@ -13,19 +14,22 @@ bool learn = false;
 int ir_code = -1;
 
 void setup() {
-  Serial.begin(115200);
+  //Serial.begin(115200);
 
-  pinMode(IR_LEARN_PIN, INPUT_PULLUP);
-  pinMode(OUT_PIN,      OUTPUT);  
+  pinMode(IR_LEARN_SW,  INPUT_PULLUP);
+  pinMode(IR_LEARN_LED, OUTPUT);  
+  pinMode(OUT_PIN,      OUTPUT); 
+  digitalWrite(IR_LEARN_LED, LOW);
   
-  if(digitalRead(IR_LEARN_PIN)==LOW){
+  if(digitalRead(IR_LEARN_SW)==LOW){
     learn = true;  
-    Serial.println("attivo learn mode!");
+    digitalWrite(IR_LEARN_LED, HIGH);
+    //Serial.println("attivo learn mode!");
     IrReceiver.begin(IR_RECEIVE_PIN);
   }else{
     ir_code = EEPROM.read(addr);
-    Serial.print("leggo ir_code: ");
-    Serial.println(ir_code);
+    //Serial.print("leggo ir_code: ");
+    //Serial.println(ir_code);
     IrReceiver.begin(IR_RECEIVE_PIN);
   }
   
@@ -35,19 +39,30 @@ void setup() {
 void loop() {
   if (IrReceiver.decode()) {
     int command = IrReceiver.decodedIRData.command;
-    Serial.println(command);
+    //Serial.println(command);
     
     if(learn){
       EEPROM.write(addr, command);
-      Serial.print("scrivo ir_code: ");
-      Serial.println(command);
+      ledBlink(5, 100, HIGH);
+      //Serial.print("scrivo ir_code: ");
+      //Serial.println(command);
     }else if(command == ir_code){
       outState = (outState == LOW) ? HIGH : LOW;
       digitalWrite(OUT_PIN, outState);
-      Serial.print("setto pin uscita: ");
-      Serial.println(outState);
+      ledBlink(1, 100, LOW);
+      //Serial.print("setto pin uscita: ");
+      //Serial.println(outState);
       delay(1000);
     }            
     IrReceiver.resume();        
   }
+}
+
+void ledBlink(int i, int d, int final_state){
+  for(int k=0; k<i; ++k){
+        delay(d);
+        digitalWrite(IR_LEARN_LED, LOW==final_state);
+        delay(d);
+        digitalWrite(IR_LEARN_LED, HIGH==final_state);
+      }
 }
